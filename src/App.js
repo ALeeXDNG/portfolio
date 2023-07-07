e098438971e9c5e7b372645b60bd3d6aa6ba357d
@AlexandruDonoaga <alexandru.donoaga@student.griffith.ie>
2023-07-07 11:32:30 +0100

    updated parallax effect

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import React,{ useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import ContactMe from "./pages/ContactMe";
-import MainNav from "./MainNav";      
-import classes from "./App.module.css";
+import React, { useState } from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import MainNav from "./MainNav";
 import SideBar from "./components/SideBar";
 
-// import planet from "./img/planet.png"
+import "./Apps.css";
 
 function App(props) {
-  const [isH1, setIsH1] = useState({ display : 'block'});
-const Heading = {
-  transform: isH1.display === 'none' ? 'translateX(100%)' : 'translateX(0)',
-  transition: 'transform 0.5s ease-in-out',
-};
- 
+  const [isH1, setIsH1] = useState({ display: "block" });
+
+  const Heading = {
+    transform: isH1.display === "none" ? "translateX(100%)" : "translateX(0)",
+    transition: "transform 0.5s ease-in-out",
+  };
 
   const handleClick = () => {
-    if (isH1.display === 'block' ) {
-      setIsH1({ display: 'none' });
+    if (isH1.display === "block") {
+      setIsH1({ display: "none" });
     } else {
-      setIsH1({ display: 'block' });
+      setIsH1({ display: "block" });
     }
-}
-
-
-
+  };
 
   return (
-    <div className={classes.mainContainer} >
-  
-     <MainNav  onClick={handleClick}   />
-     <SideBar style={isH1.display === 'block' ? { display: 'none' } : { display: 'block' }}
-     />
-      {/* <img className={classes.planet} src={planet} alt="planet" />{" "} */}
-      <div style={Heading} >  
-      <h1      style={isH1}   >Welcome to my world</h1>
-        </div>
+    <div>
+      <div className="mainContainer">
+        <Parallax pages={4} pointerEvents={true}>
+          <ParallaxLayer offset={0} speed={0.6}>
+            <div className="animation_layer parallax" id="mountain1"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.4}>
+            <div className="animation_layer parallax" id="mountain2"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.1}>
+            <div style={Heading}>
+              <h1> Welcome to my world</h1>
+            </div>
+          </ParallaxLayer>
+        </Parallax>
+        <MainNav onClick={handleClick} />
+        <SideBar
+          style={
+            isH1.display === "block"
+              ? { display: "none" }
+              : { display: "block" }
+          }
+        />
+      </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
